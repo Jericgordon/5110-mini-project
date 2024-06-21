9cc367c0811cc592fa8366f97c599e19fec470ba
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2024-06-20 20:22:39 -0400

    added changes to readme

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from File_imports import read_file
 from Data_Preprocessing import merge_files_and_format
 from matplotlib import pyplot as plt
-from NaN import *
+from NaN import fix_NaN
+
 
 def main():
     df1 = read_file("./Data/noisy_data.db",table_name = "noisy_table")
@@ -13,7 +14,7 @@ def main():
     df_list = [df1,df2,df3,df4,df5]
     df_merge = merge_files_and_format(df_list)
     df = fix_NaN(df_merge)
-    #print(df)
+    
 
 
 
